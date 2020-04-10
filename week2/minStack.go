5b3ebd377d7c6ea3b3279e13b886a2bfcaaee8ac
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-04-10 19:03:19 +0100

    Min stack tested and implemented

--- week2/minStack.go
+++ week2/minStack.go
@@ -1,33 +1,53 @@
 package main;
 
 type MinStack struct {
-    
+	min int;
+	items []int;
 }
 
+const MaxUint = ^uint(0) 
+const MinUint = 0 
+const MaxInt = int(MaxUint >> 1)
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
-    return MinStack {};
+    return MinStack { items: []int {}, min: MaxInt};
 }
-
 
 func (this *MinStack) Push(x int)  {
-    
-}
+	this.items = append(this.items, x);
 
+	if this.min > x {
+		this.min = x;
+	}
+}
 
 func (this *MinStack) Pop()  {
-    
-}
 
+	top := this.Top();
+	this.items = this.items[0:len(this.items) -1];
+
+	if top == this.min {
+		min := MaxInt
+		for _, n := range this.items{
+			if min > n {
+				min = n;
+			}
+		}		
+		this.min = min;
+	}
+}
 
 func (this *MinStack) Top() int {
-    return 0;
+    return this.items[len(this.items) - 1];
 }
 
-
 func (this *MinStack) GetMin() int {
-    return 0;
+      return this.min;
+}
+
+func main(){
+
 }
 
 
--- week2/minStack_test.go
+++ week2/minStack_test.go
@@ -14,8 +14,8 @@ func TestGetMinInStack(t *testing.T) {
 	for _, table := range tables {
 		obj := Constructor();
 		
-		param_3 := obj.Top();
-		param_4 := obj.GetMin();
+		// param_3 := obj.Top();
+		// param_4 := obj.GetMin();
 
 		for _, n := range table.a {
 			obj.Push(n);
@@ -23,7 +23,60 @@ func TestGetMinInStack(t *testing.T) {
 		actualMin:= obj.GetMin();
 
         if actualMin != table.exp {
-			t.Errorf("%d, min not equal not equal %s, should be %d", table.a, actualMin, table.exp);
+			t.Errorf("%d, min not equal %d, should be %d", table.a, actualMin, table.exp);
+         }
+	}
+}
+
+func TestPopTopMinStack(t *testing.T) {
+	tables := []struct {
+		a []int
+		exp int
+	}{
+		{ []int {1,2,3}, 2},
+		{ []int {2,3,4}, 3},
+    }
+    
+	for _, table := range tables {
+		obj := Constructor();
+		
+		// param_3 := obj.Top();
+		// param_4 := obj.GetMin();
+
+		for _, n := range table.a {
+			obj.Push(n);
+		}
+		obj.Pop();
+		top:= obj.Top();
+
+        if top != table.exp {
+			t.Errorf("%d, top not equal %d, should be %d", table.a, top, table.exp);
+         }
+	}
+}
+
+
+func TestPopMinStack(t *testing.T) {
+	tables := []struct {
+		a []int
+		exp int
+	}{
+		{ []int {3,2,1}, 2},
+		{ []int {4,3,1}, 3},
+    }
+    
+	for _, table := range tables {
+		obj := Constructor();
+		
+		for _, n := range table.a {
+			obj.Push(n);
+		}
+		obj.Pop();
+
+		actualMin:= obj.GetMin();
+
+        if actualMin != table.exp {
+			t.Errorf("%d, min not equal %d, should be %d", table.a, actualMin, table.exp);
          }
 	}
 }
