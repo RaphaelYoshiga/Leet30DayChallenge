76b5f8f790ab28fe25e9827c7dc56611bc9a2e61
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-04-22 19:38:47 +0100

    Sub Array SUM.go

---
+++ week4/subArraySum.go
@@ -0,0 +1,24 @@
+package main
+
+func subarraySum(nums []int, k int) int {
+	m := make(map[int]int);
+
+	m[0] = 1;
+	sum := 0;
+	result := 0;
+
+	for _, n:= range nums{
+		sum+= n;
+		if m[sum -k] > 0{
+			result+= m[sum -k];
+		}
+
+		m[sum] += 1;
+	}
+
+	return result;
+}
+
+func main(){
+	subarraySum([]int { 1, 1, 1, -1}, 2)
+}
