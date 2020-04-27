e30744a07ff3f01d46acbeea84ba4665886a6fd1
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-04-27 19:34:46 +0100

    Square with dynamic programming

--- week4/maximalSquare.go
+++ week4/maximalSquare.go
@@ -1,8 +1,56 @@
 package main;
 
 func maximalSquare(matrix [][]byte) int {   
-	squareSize:= getMaxSquareSize(matrix);
+	squareSize:= getMaxSquareSizeDP(matrix);
 	return squareSize * squareSize;
+}
+
+func getMaxSquareSizeDP(matrix [][]byte) int{
+	maxSize:= 0;
+
+	var dp = make([][]int, len(matrix))
+	for i := range dp {
+		dp[i] = make([]int, len(matrix[0]))
+	}
+	
+	for i, row := range matrix{
+		for y, cell := range row{
+			if y == 0 || i == 0{
+				val:= cast(cell);
+				dp[i][y] = val; 
+				maxSize = max(val, maxSize);
+			} else{
+
+				if cell == '1'{
+					left:= dp[i][y-1];
+					top:= dp[i-1][y];
+					topLeft:= dp[i-1][y -1];
+	
+					neighboursMin := min(min(left, top), topLeft)
+					val := neighboursMin + 1;
+					dp[i][y] = val;
+					maxSize = max(val, maxSize);
+				}else{
+					dp[i][y] = 0;
+				}
+			}
+		}
+	}
+	return maxSize;
+}
+
+func min(a int, b int) int{
+	if a > b {
+		return b;
+	}
+	return a;
+}
+
+func cast(cell byte) int{
+	if cell == '1'{
+		return 1;
+	}
+	return 0;
 }
 
 func getMaxSquareSize(matrix [][]byte) int{
@@ -68,15 +116,10 @@ func max(a int, b int) int{
 }
 
 func main(){
-	bytes:= [][]byte {
-        {'0','1','1','0','0','1','0','1','0','1'},
-        {'0','0','1','0','1','0','1','0','1','0'},
-        {'1','0','0','0','0','1','0','1','1','0'},
-        {'0','1','1','1','1','1','1','0','1','0'},
-        {'0','0','1','1','1','1','1','1','1','0'},
-        {'1','1','0','1','0','1','1','1','1','0'},
-        {'0','0','0','1','1','0','0','0','1','0'},
-        {'1','1','0','1','1','0','0','1','1','1'},
-        {'0','1','0','1','1','0','1','0','1','1'}};
+    bytes := [][]byte{ 
+		{'1','1','1'},
+		{'1','0','1'},
+		{'1','1','1'},
+	};
 	maximalSquare(bytes);
 }
